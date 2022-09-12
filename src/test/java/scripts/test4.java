a3aac506b5c651af882e85374af02206e4b8a3dd
leo <leo@nitin>
2022-09-13 01:03:45 +0530

    hi

---
+++ src/test/java/scripts/test4.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test4 {
+	@Test
+	public void tc1() {
+		
+		Reporter.log("hello pythonselenium",true);
+	}
+
+}
